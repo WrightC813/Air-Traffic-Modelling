787b141a2c6a17c456441f8b54c308e0297e291e
WrightC813 <Wright_Christopher_77@proton.me>
2026-04-01 22:45:16 +0100

    Imporved data scraper code, though still blocked sometimes by website

--- airport_data_scraper.py
+++ airport_data_scraper.py
@@ -9,6 +9,14 @@ import bs4
 from playwright.sync_api import sync_playwright
 from playwright.sync_api import expect
 import pandas as pd
+import random
+import time
+import datetime
+
+mean_delay = 1
+long_delay = 9
+date_dict = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6,
+             'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
 
 
 # %% Main function which takes in high level info and refurns data frame with from this page
@@ -69,8 +77,11 @@ def arrive_date_tag(tag):
     
 def set_page(page, num_pages, target_page):
     if num_pages == 1:
-        return page
-    expect(page.get_by_text('→', exact=True).first).to_be_visible()
+        return page, False
+    try:
+        expect(page.get_by_text('→', exact=True).first).to_be_visible()
+    except:
+        return page, True
     visible_buttons = []
     for i in range(num_pages):
         if page.get_by_text(f'{i+1}', exact=True).first.is_visible():
@@ -78,53 +89,84 @@ def set_page(page, num_pages, target_page):
             
     if target_page in visible_buttons:
         page.get_by_text(f'{target_page}', exact=True).first.click()
-        return page
+        return page, False
     
     elif target_page < min(visible_buttons):
         for j in range(min(visible_buttons) - target_page):
+            time.sleep(random.expovariate(mean_delay))
             page.get_by_text(f'{min(visible_buttons) - j}', exact=True).first.click()
             expect(page.get_by_text(f'{min(visible_buttons) - j - 1}', exact=True).first).to_be_visible()
 
         page.get_by_text(f'{target_page}', exact=True).first.click()
-        return page
+        return page, False
     
     else:
         for j in range(target_page - max(visible_buttons)):
+            time.sleep(random.expovariate(mean_delay))
             page.get_by_text(f'{max(visible_buttons) + j}', exact=True).first.click()
             expect(page.get_by_text(f'{max(visible_buttons) + j + 1}', exact=True).first).to_be_visible()
             
         page.get_by_text(f'{target_page}', exact=True).first.click()
-        return page
+        return page, False
     
     
-def get_flight_data(page, ind, num_pages, tag):
-    set_page(page, num_pages, ind)
+def get_flight_data(page, ind, num_pages, tag, departure):
+    page, blocked = set_page(page, num_pages, ind)
+    if blocked:
+        return page, pd.DataFrame(), True
 
     href = tag.get('href')
-    print(href)
-    row_tag = page.locator(f'a[href="{href}"]')
+    flight_code = ' '.join(href.split('?')[0].split('/')[-2:])
+    
+    time.sleep(random.expovariate(mean_delay))
     try:
-        expect(row_tag).to_be_visible()
+        page.locator(f'a[href="{href}"]').click()
     except:
-        print(f'Link {href} failed')
-        return None
-    row_tag.click()            
+        print(f'{flight_code} failed, could not click flight row')
+        return page, pd.DataFrame(), True  
     expect(page.locator('#extendedDetailsButton')).to_be_visible()
     html2 = page.content()
     soup2 = bs4.BeautifulSoup(html2, 'html.parser')
+        
+    if flight_code not in soup2.get_text():
+        print(f'{flight_code} failed, code not found on page')
+        return page, pd.DataFrame(), True
+    
+    if 'Flight Cancelled' in soup2.get_text():
+        print(f'{flight_code} failed, flight cancelled')
+        return page, pd.DataFrame(), False
     
     time_tags = soup2.find_all(time_tag)
     depart_date = soup2.find(depart_date_tag)
     arrival_date = soup2.find(arrive_date_tag)
     
-    return page, time_tags, depart_date, arrival_date
-    
-    
-            
+    try:
+        depart_tag, arrival_tag = time_tags[:2]
+        if departure:
+            ti = depart_tag.contents[0]
+            da = depart_date.contents[0]
+            tz = depart_tag.contents[1].contents[0]
+            inc = -1
+        else:
+            ti = arrival_tag.contents[0]
+            da = arrival_date.contents[0]
+            tz = arrival_tag.contents[1].contents[0]
+            inc = 1
+        t = ti.split(':')
+        d = da.split('-')
+        dts = datetime.datetime(int(d[2]), date_dict[d[1]], int(d[0]), int(t[0]), int(t[1]))
+        df = pd.DataFrame([{'Flight Code':flight_code,'DateTime':dts, 'Time Zone':tz,  'Increment':inc}])
+        print(flight_code)
+        return page, df, False
+    except:
+        print(f'{flight_code} failed, tags contain no time data')
+        return page, pd.DataFrame(), False
+        
     
 
 def get_data(departure, airport, year, month, day, hour):
     link = gen_link(departure, airport, year, month, day, hour)
+    blocked_overall = False
     
     #launch browser
     playwright = sync_playwright().start()
@@ -134,6 +176,7 @@ def get_data(departure, airport, year, month, day, hour):
 
     #clear cookie popup
     try:
+        time.sleep(random.expovariate(mean_delay))
         expect(page.get_by_role("button", name='Accept All Cookies')).to_be_visible()
         page.get_by_role('button', name='Accept All Cookies').click()
     except:
@@ -151,54 +194,92 @@ def get_data(departure, airport, year, month, day, hour):
 
     #find number of pages
     num_pages = 1
-
     soup = bs4.BeautifulSoup(page.content(),'html.parser')
-
-    
-
     nav_buttons = soup.find_all(get_nav_buttons)
     for b in nav_buttons:
         if str(b.text).isdigit():
             num_pages = max(num_pages,int(b.text))
 
     
-
-    df = pd.DataFrame(columns=['Time', 'Time Zone', 'Date', 'Increment'])
+    df = pd.DataFrame(columns=['Flight Code', 'DateTime', 'Time Zone', 'Increment'])
     failed_links = []
 
     for i in range(num_pages):
+        print(f'Page {i+1} of {num_pages}:')
         set_page(page, num_pages, i+1)
 
         soup1 = bs4.BeautifulSoup(page.content(),'html.parser')
         selected = soup1.find_all(table_tag)
         for tag in selected:
             try:
-                page, time_tags, depart_date, arrival_date = get_flight_data(page, i+1, num_pages, tag)
+                page, df_t, blocked = get_flight_data(page, i+1, num_pages, tag, departure)
             except:
-                print(f'{tag.get("href")} failed')
+                print('Failed to get data')
                 failed_links.append(tag.get("href"))
+                time.sleep(random.expovariate(mean_delay))
                 page.go_back()
                 continue
             
-            try:
-                depart_tag, arrival_tag = time_tags[:2]
-                if departure:
-                    df = pd.concat([df,pd.DataFrame([{'Time':depart_tag.contents[0], 'Time Zone':depart_tag.contents[1].contents[0], 'Date':depart_date.contents[0], 'Increment':-1}])], ignore_index=True)
-                else:
-                    df = pd.concat([df,pd.DataFrame([{'Time':arrival_tag.contents[0], 'Time Zone':arrival_tag.contents[1].contents[0], 'Date':arrival_date.contents[0], 'Increment':1}])], ignore_index=True)
-            except:
-                print(f'{tag.get("href")} failed')
-                failed_links.append(tag.get("href"))
+            if blocked:
+                print('Suspect website blocked us')
+                blocked_overall = True
+                break
             
+            try:
+                df = pd.concat([df,df_t], ignore_index=True)
+            except:
+                print(f'{tag.get("href")} failed to add data')
+                failed_links.append(tag.get("href"))
+            time.sleep(random.expovariate(mean_delay))
             page.go_back()
+        if blocked_overall:
+            break
     
     browser.close()
     playwright.stop()
-    return df, failed_links
+    return df, failed_links, blocked_overall
     
-df_a, failed_a = get_data(True, 'GLA', 2026, 3, 28, 6)
-print(df_a)
-print(failed_a)
-#df_b , failed_b = get_data(False, 'BHX', 2026, 3, 27, 6)
-#print(df_b)
-#print(failed_b)
+
+df = pd.DataFrame()
+failed = []
+airport = 'KOI'
+
+to_retry = []
+
+for dept in (True, False):
+    for i in range(4):
+        if dept:
+            print(f'Departures from {6*i}h to {6*(i+1)}h')
+        if not dept:
+            print(f'Arrivals from {6*i}h to {6*(i+1)}h')
+        try:
+            df_t, failed_t, blocked = get_data(dept, airport, 2026, 3, 31, 6*i)
+            if blocked:
+                to_retry.append((dept,i))
+                time.sleep(long_delay + random.expovariate(mean_delay))
+                continue
+            failed = failed + failed_t
+            df = pd.concat([df,df_t], ignore_index=True)
+        except:
+            print('No data extracted')
+
+if len(to_retry) > 0:
+    print('Retrying blocked pages')
+    time.sleep(long_delay + random.expovariate(mean_delay))
+    for p in to_retry:
+        dept, i = p
+        if dept:
+            print(f'Departures from {6*i}h to {6*(i+1)}h')
+        if not dept:
+            print(f'Arrivals from {6*i}h to {6*(i+1)}h')
+        try:
+            df_t, failed_t, blocked = get_data(dept, airport, 2026, 3, 30, 6*i)
+            if blocked:
+                print('Blocked again')
+            failed = failed + failed_t
+            df = pd.concat([df,df_t], ignore_index=True)
+        except:
+            print('No data extracted')
+
+print(df.sort_values('DateTime'))
+print(failed)
